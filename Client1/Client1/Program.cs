11cb44f1d0e1b7619fcaafd8ff8acdad56701571
Hudaynazar <nazariskabulov@gmail.com>
2025-07-03 22:13:11 +0300

    first commit

---
+++ Client1/Client1/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient tcpClient = new TcpClient("localhost", XXXX); //burada portunuz
+
+                SslStream sslStream = new SslStream(
+                    tcpClient.GetStream(),
+                    false,
+                    new RemoteCertificateValidationCallback(ValidateServerCertificate),
+                    null);
+
+                sslStream.AuthenticateAsClient("localhost");
+
+                Console.WriteLine("Sunucuya güvenli bağlantı sağlandı.");
+
+                Task.Run(() => SendMessage(sslStream));
+
+                while (true)
+                {
+                    if (!IsConnected(tcpClient))
+                    {
+                        Console.WriteLine("Bağlantı koptu!");
+                        break;
+                    }
+
+                    try
+                    {
+                        string gelen = ReceiveMessage(sslStream);
+                        if (gelen == null)
+                            break;
+
+                        Console.WriteLine("Server: " + gelen);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("İletişim hatası: " + ex.Message);
+                    }
+                }
+
+                tcpClient.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Sunucuya bağlanılamadı: " + ex.Message);
+            }
+        }
+
+        // Sertifika doğrulaması (şu anlık her şeyi kabul ediyoruz)
+        public static bool ValidateServerCertificate(
+            object sender,
+            X509Certificate certificate,
+            X509Chain chain,
+            SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+
+        static string ReceiveMessage(SslStream stream)
+        {
+            try
+            {
+                byte[] uzunlukBytes = new byte[4];
+                stream.Read(uzunlukBytes, 0, 4);
+                if (BitConverter.IsLittleEndian)
+                    Array.Reverse(uzunlukBytes);
+
+                int uzunluk = BitConverter.ToInt32(uzunlukBytes, 0);
+                byte[] mesajBytes = new byte[uzunluk];
+                int toplamOkunan = 0;
+
+                while (toplamOkunan < uzunluk)
+                {
+                    int okunan = stream.Read(mesajBytes, toplamOkunan, uzunluk - toplamOkunan);
+                    if (okunan == 0) break;
+                    toplamOkunan += okunan;
+                }
+
+                return Encoding.UTF8.GetString(mesajBytes);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Mesaj alma hatası: " + ex.Message);
+                return null;
+            }
+        }
+
+        static void SendMessage(SslStream stream)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("> ");
+                    string mesaj = Console.ReadLine();
+
+                    byte[] mesajBytes = Encoding.UTF8.GetBytes(mesaj);
+                    int mesajUzunlugu = mesajBytes.Length;
+                    byte[] uzunlukBytes = BitConverter.GetBytes(mesajUzunlugu);
+                    if (BitConverter.IsLittleEndian)
+                        Array.Reverse(uzunlukBytes);
+
+                    byte[] gonderilecek = new byte[4 + mesajUzunlugu];
+                    Array.Copy(uzunlukBytes, 0, gonderilecek, 0, 4);
+                    Array.Copy(mesajBytes, 0, gonderilecek, 4, mesajUzunlugu);
+
+                    stream.Write(gonderilecek);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Gönderme hatası: " + ex.Message);
+                    break;
+                }
+            }
+        }
+
+        static bool IsConnected(TcpClient client)
+        {
+            try
+            {
+                if (client == null || !client.Connected)
+                    return false;
+
+                if (client.Client.Poll(0, SelectMode.SelectRead) && client.Client.Available == 0)
+                    return false;
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Server1/Server1/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+
+namespace Server1
+{
+    internal class Program
+    {
+        static Dictionary<string, SslStream> clientStreams = new Dictionary<string, SslStream>();
+        static object clientLock = new object();
+
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("XXX.XXX.XXX.XXX"); //BURAYA SİZİN YEREL İP'niz GELİR
+            int port = XXXX; //Buraya portunuz
+
+            try
+            {
+                TcpListener server = new TcpListener(ip, port);
+                server.Start();
+                Console.WriteLine("Sunucu başlatıldı. Bağlantı bekleniyor...");
+
+                int clientCount = 0;
+
+                Task.Run(() => ServerConsoleInput());
+
+                X509Certificate2 certificate = new X509Certificate2("cert.pfx", "SZİN ŞİFRENİZ"); //BURAYA SİZİN ŞİFRENİZ GELMEDLİDİR
+
+                while (true)
+                {
+                    TcpClient client = null;
+                    try
+                    {
+                        client = server.AcceptTcpClient();
+                        clientCount++;
+                        string clientName = "Client" + clientCount;
+
+                        SslStream sslStream = new SslStream(client.GetStream(), false);
+                        sslStream.AuthenticateAsServer(certificate, false, false);
+
+                        lock (clientLock)
+                        {
+                            clientStreams.Add(clientName, sslStream);
+                        }
+
+                        Console.WriteLine($"{clientName} bağlandı.");
+
+                        Task.Run(() => HandleClient(client, sslStream, clientName));
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("İstemci Bağlanamadı: " + ex.ToString());
+                        client?.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Sunucu Başlatılamadı: " + ex.ToString());
+            }
+        }
+
+        static void HandleClient(TcpClient client, SslStream sslStream, string clientName)
+        {
+            try
+            {
+                while (true)
+                {
+                    if (!IsConnected(client))
+                    {
+                        Console.WriteLine("Bağlantı Koptu");
+                        break;
+                    }
+
+                    string gelen = ReceiveMessage(sslStream);
+                    if (gelen == null)
+                        break;
+
+                    Console.WriteLine($"{clientName} dedi ki: {gelen}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Veri Alınamadı: " + ex.Message);
+            }
+            finally
+            {
+                lock (clientLock)
+                {
+                    clientStreams.Remove(clientName);
+                }
+                client.Close();
+                Console.WriteLine($"{clientName} bağlantısı kapandı.");
+            }
+        }
+
+        static string ReceiveMessage(SslStream sslStream)
+        {
+            try
+            {
+                byte[] uzunlukBytes = new byte[4];
+                sslStream.Read(uzunlukBytes, 0, 4);
+                if (BitConverter.IsLittleEndian)
+                    Array.Reverse(uzunlukBytes);
+
+                int uzunluk = BitConverter.ToInt32(uzunlukBytes, 0);
+
+                byte[] messageByte = new byte[uzunluk];
+                int toplamOkunan = 0;
+
+                while (toplamOkunan < uzunluk)
+                {
+                    int okunan = sslStream.Read(messageByte, toplamOkunan, uzunluk - toplamOkunan);
+                    if (okunan == 0)
+                        break;
+                    toplamOkunan += okunan;
+                }
+                return Encoding.UTF8.GetString(messageByte);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Mesaj alma hatası: " + ex.ToString());
+                return null;
+            }
+        }
+
+        static void SendMessage(SslStream sslStream, string message)
+        {
+            try
+            {
+                byte[] mesajBytes = Encoding.UTF8.GetBytes(message);
+                int mesajUzunluğu = mesajBytes.Length;
+
+                byte[] uzunlukBytes = BitConverter.GetBytes(mesajUzunluğu);
+                if (BitConverter.IsLittleEndian)
+                    Array.Reverse(uzunlukBytes);
+
+                byte[] gonderilecek = new byte[uzunlukBytes.Length + mesajUzunluğu];
+                Array.Copy(uzunlukBytes, 0, gonderilecek, 0, uzunlukBytes.Length);
+                Array.Copy(mesajBytes, 0, gonderilecek, uzunlukBytes.Length, mesajUzunluğu);
+
+                sslStream.Write(gonderilecek, 0, gonderilecek.Length);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Mesaj gönderme hatası: " + ex);
+            }
+        }
+
+        static void ServerConsoleInput()
+        {
+            while (true)
+            {
+                Console.WriteLine("Kime mesaj göndermek istiyorsun? (Mevcutlar: " + string.Join(", ", clientStreams.Keys) + ")");
+                Console.Write("İsim: ");
+                string hedef = Console.ReadLine();
+
+                lock (clientLock)
+                {
+                    if (clientStreams.ContainsKey(hedef))
+                    {
+                        Console.Write("Mesaj: ");
+                        string mesaj = Console.ReadLine();
+                        SendMessage(clientStreams[hedef], mesaj);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bu isimde bir istemci yok.");
+                    }
+                }
+            }
+        }
+
+        static bool IsConnected(TcpClient client)
+        {
+            try
+            {
+                if (client == null || !client.Connected)
+                    return false;
+
+                if (client.Client.Poll(0, SelectMode.SelectRead) && client.Client.Available == 0)
+                    return false;
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
